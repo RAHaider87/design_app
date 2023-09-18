9ed05578b6353600c9fb216e74df69ab257ef213
Ali Haider <ali.r4565081@gmail.com>
2023-09-18 16:00:07 +0500

    nav

--- src/components/chefs/messageChefs/index.js
+++ src/components/chefs/messageChefs/index.js
@@ -1,38 +1,36 @@
-import { Box, Grid } from "@mui/material";
-import { Typography } from "antd";
+import { Grid, Typography } from "@mui/material";
+import { useNavigate } from 'react-router-dom';
+
 
 export default function MessageChefs() {
-    return (
+    const navigate = useNavigate();
 
-        <Grid container sx={{
+    return (
+        <Grid container onClick={() => navigate('/home')} sx={{
             maxWidth: '732px',
             width: '100%',
-            height: '54px'
+            height: '54px',
+            backgroundColor: '#FF611D',
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            borderRadius: '10px 0 10px 0',
+            cursor: 'pointer',
         }}>
             <Grid item xs={12} sm={12} md={12} lg={12} xl={12}>
-                <Box sx={{
+                <Typography style={{
                     maxWidth: '100%',
                     width: '100%',
-                    height: '100%',
-                    backgroundColor: '#FF611D',
-                    display: 'flex',
-                    justifyContent: 'center',
-                    alignItems: 'center',
-                    borderRadius: '10px 0 10px 0',
-                }}>
-                    <Typography style={{
-                        maxWidth: '109px',
-                        width: '100%',
-                        height: '29px',
-                        fontFamily: 'Work Sans',
-                        fontWeight: 500,
-                        fontSize: '16px',
-                        lineHeight: '29px',
-                        color: '#FFFFFF',
-                    }}>
-                        Message Chef
-                    </Typography>
-                </Box>
+                    height: '27px',
+                    fontFamily: 'Work Sans',
+                    fontWeight: 500,
+                    fontSize: '16px',
+                    lineHeight: '29px',
+                    color: '#FFFFFF',
+                    textAlign: 'center',
+                }} >
+                    Message Chef
+                </Typography>
             </Grid>
         </Grid>
     )
--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,4 +1,4 @@
-import { AppBar, Box, Button, Grid, Typography } from "@mui/material";
+import { AppBar, Box, Button, Container, Grid, Typography, colors } from "@mui/material";
 import logoImage from '../../naveBarLogo/NavLogo.png';
 import { useNavigate, useLocation } from 'react-router-dom';
 import Link from '@mui/material/Link';
@@ -14,40 +14,68 @@ export default function Header() {
     };
 
     return (
-        <AppBar sx={{
-            backgroundColor: '#ffffff',
-            position: 'static',
-            width: '100%',
-            maxWidth: '1920px',
-            margin: '0 auto',
-            height: '100px',
-            '@media only screen and (max-width: 1820px)': {
-                maxWidth: '1800px',
-            },
-            '@media only screen and (max-width: 1680px)': {
-                maxWidth: '1680px',
-            },
-            '@media only screen and (max-width: 1550px)': {
-                maxWidth: '1560px',
-            },
-            '@media only screen and (max-width: 1410px)': {
-                maxWidth: '1440px',
-            }
-        }}>
-            {/* <Container> */}
-            <Box className="contentBox">
-                {/* <Grid container alignItems="center"> */}
-                <Box className='logoBox'>
-                    <img src={logoImage} alt="Logo" style={{ width: '100%', maxWidth: '124px', height: '80px' }} />
-                </Box>
 
-                <Box className='linksBox' >
+
+
+
+
+
+        // '@media only screen and (max-width: 1820px)': {
+        //     maxWidth: '1800px',
+        // },
+        // '@media only screen and (max-width: 1680px)': {
+        //     maxWidth: '1680px',
+        // },
+        // '@media only screen and (max-width: 1550px)': {
+        //     maxWidth: '1560px',
+        // },
+        // '@media only screen and (max-width: 1410px)': {
+        //     maxWidth: '1440px',
+        // }
+
+
+        <Grid container sx={{
+            backgroundColor: Colors.Blue,
+            position: 'static',
+            maxWidth: '1920px',
+            width: '100%',
+            height: '100px',
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center', 
+            margin:'auto'
+
+
+        }}>
+            <Grid item sx={{
+                maxWidth: '1680px',
+                width: '100%',
+                height: '79.80px',
+                backgroundColor: Colors.denim_Blue,
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center'
+
+            }}>
+
+                <Grid item xs={2} sm={2} md={2} lg={2} xl={2} sx={{
+                    maxWidth: '124px',
+                    width: '100%',
+                    height: '79.80px',
+                    backgroundColor:Colors.green
+                }}>
+                    <img src={logoImage} alt="Logo" style={{ width: '100%', height: '100%' }} />
+                </Grid>
+
+                <Grid item xs={10} sm={10} md={10} lg={10} xl={10} sx={{
+                    display: 'flex',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                    backgroundColor:Colors.russian_green
+                }} >
                     <Typography variant="body1"
                         sx={{
-                            color: isRouteActive('/') ? '#FD6288' : 'black',
-                            '&:hover': {
-                                cursor: 'pointer'
-                            }
+                            color: isRouteActive('/') ? '#FD6288' : 'black'
                         }}>
                         <Link onClick={() => navigate('/')} underline="none" color="inherit">
                             Home
@@ -56,10 +84,7 @@ export default function Header() {
 
                     <Typography variant="body1"
                         sx={{
-                            color: isRouteActive('/health') ? '#FD6288' : 'black',
-                            '&:hover': {
-                                cursor: 'pointer'
-                            }
+                            color: isRouteActive('/health') ? '#FD6288' : 'black'
                         }}>
                         <Link onClick={() => navigate('/health')} underline="none" color="inherit">
                             Health
@@ -68,10 +93,7 @@ export default function Header() {
 
                     <Typography variant="body1"
                         sx={{
-                            color: isRouteActive('/aboutUs') ? '#FD6288' : 'black',
-                            '&:hover': {
-                                cursor: 'pointer'
-                            }
+                            color: isRouteActive('/aboutUs') ? '#FD6288' : 'black'
                         }}>
                         <Link onClick={() => navigate('/aboutUs')} underline="none" color="inherit">
                             About Us
@@ -80,10 +102,7 @@ export default function Header() {
 
                     <Typography variant="body1"
                         sx={{
-                            color: isRouteActive('/career&finance') ? '#FD6288' : 'black',
-                            '&:hover': {
-                                cursor: 'pointer'
-                            }
+                            color: isRouteActive('/career&finance') ? '#FD6288' : 'black'
                         }}>
                         <Link onClick={() => navigate('/career&finance')} underline="none" color="inherit">
                             Career & Finance
@@ -92,10 +111,7 @@ export default function Header() {
 
                     <Typography variant="body1"
                         sx={{
-                            color: isRouteActive('/family') ? '#FD6288' : 'black',
-                            '&:hover': {
-                                cursor: 'pointer'
-                            }
+                            color: isRouteActive('/family') ? '#FD6288' : 'black'
                         }}>
                         <Link onClick={() => navigate('/family')} underline="none" color="inherit">
                             Family
@@ -104,10 +120,7 @@ export default function Header() {
 
                     <Typography variant="body1"
                         sx={{
-                            color: isRouteActive('/food') ? '#FD6288' : 'black',
-                            '&:hover': {
-                                cursor: 'pointer'
-                            }
+                            color: isRouteActive('/food') ? '#FD6288' : 'black'
                         }}>
                         <Link onClick={() => navigate('/food')} underline="none" color="inherit">
                             Food
@@ -116,10 +129,7 @@ export default function Header() {
 
                     <Typography variant="body1"
                         sx={{
-                            color: isRouteActive('/wellness') ? '#FD6288' : 'black',
-                            '&:hover': {
-                                cursor: 'pointer'
-                            }
+                            color: isRouteActive('/wellness') ? '#FD6288' : 'black'
                         }}>
                         <Link onClick={() => navigate('/wellness')} underline="none" color="inherit">
                             Wellness
@@ -128,10 +138,7 @@ export default function Header() {
 
                     <Typography variant="body1"
                         sx={{
-                            color: isRouteActive('/sex&relationship') ? '#FD6288' : 'black',
-                            '&:hover': {
-                                cursor: 'pointer'
-                            }
+                            color: isRouteActive('/sex&relationship') ? '#FD6288' : 'black'
                         }}>
                         <Link onClick={() => navigate('/sex&relationship')} underline="none" color="inherit">
                             Sex & Relationship
@@ -140,10 +147,7 @@ export default function Header() {
 
                     <Typography variant="body1"
                         sx={{
-                            color: isRouteActive('/beauty') ? '#FD6288' : 'black',
-                            '&:hover': {
-                                cursor: 'pointer'
-                            }
+                            color: isRouteActive('/beauty') ? '#FD6288' : 'black'
                         }}>
                         <Link onClick={() => navigate('/beauty')} underline="none" color="inherit">
                             Beauty
@@ -156,10 +160,11 @@ export default function Header() {
                         </Button>
                         <Button variant="outlined" sx={{ ml: '18px' }}>SignUp</Button>
                     </Box>
-                </Box>
-                {/* </Grid> */}
-            </Box >
-            {/* </Container> */}
-        </AppBar>
+                </Grid>
+            </Grid>
+
+        </Grid >
+
+
     )
 }
